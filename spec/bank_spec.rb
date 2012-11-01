b49f687e0f7190ac7fb2ed242bedea0aac9598ec
Kerry Buckley <kerryjbuckley@gmail.com>
2012-11-01 22:24:42 +0000

    Tidy up spec slightly to make next step simpler (cheat!)

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -2,19 +2,22 @@ require "bank"
 
 describe Bank do
   subject(:bank) { Bank.new }
-  let(:account) { stub }
-  before { Account.stub(:new).with("fred").and_return account }
+  let(:fred_account) { stub("Fred's account") }
+
+  before do
+    Account.stub(:new).with("fred").and_return fred_account
+  end
 
   describe "#create_account" do
     it "returns a new account" do
-      bank.create_account("fred").should == account
+      bank.create_account("fred").should == fred_account
     end
   end
 
   describe "#account_for" do
     it "retrieves the named account" do
       bank.create_account("fred")
-      bank.account_for("fred").should == account
+      bank.account_for("fred").should == fred_account
     end
 
     it "throws an exception if no account exists" do
